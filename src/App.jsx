6c30f512d1dc176471b41668fb7b947b29cf0678
shahid-ahmed1 <shahidcse2122gmail,com>
2025-01-06 09:11:45 +0600

    created fake data json format and usestate useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from "./components/header/Header"
 
 function App() {
   
@@ -5,7 +6,7 @@ function App() {
   return (
     <>
       
-      <h1 className='text-2xl text-red-600'>Vite + React</h1>
+       <Header></Header>
      
     </>
   )
